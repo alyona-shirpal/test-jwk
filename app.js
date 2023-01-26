36b287f55b7b1dda0c1e4612656adbd48652ce6f
alyona-shirpal <shirpalalona@gmail.com>
2023-01-26 11:13:55 +0200

    JWT with RSA key-pair in micro-service

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const createError = require('http-errors');
+const morgan = require('morgan');
+require('dotenv').config();
+const { expressjwt: jwt } = require('express-jwt');
+const jwkClient = require('jwks-rsa');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan('dev'));
+app.use(jwt(
+    {
+        secret: jwkClient.expressJwtSecret({
+        jwksUri: 'http://localhost:4000/.well-known/jwk.json',
+        cache: true,
+        rateLimit: true
+      }),
+      algorithms: ['RS256']
+    }
+).unless({ path: ['/'] }))
+
+app.get('/', async (req, res, next) => {
+  res.send({ message: 'Awesome it works 🐻' });
+});
+app.get('/protected', async (req, res, next) => {
+
+
+  res.send({ message: 'THis is protected route 🐻' });
+});
+
+app.use('/api', require('./routes/api.route'));
+
+app.use((req, res, next) => {
+  next(createError.NotFound());
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    status: err.status || 500,
+    message: err.message,
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`🚀 @ http://localhost:${PORT}`));
